73e4615997c864f6c0302c4f49d5b48f6fe2b9d7
whoami9203 <39827915+whoami9203@users.noreply.github.com>
2025-05-11 15:48:41 +0800

    environment setup

---
+++ main.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import os
+from tqdm import tqdm
+
+
+def main():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
